d38a5e23d295fc2fc7c1c34c95e80c5d9eb517e9
João Fouyer <jfouyer@gmail.com>
2023-09-29 02:27:41 -0300

    typo fix

--- src/main/java/me/fouyer/Main.java
+++ src/main/java/me/fouyer/Main.java
@@ -3,7 +3,7 @@ package me.fouyer;
 public class Main {
     public static void main(String[] args) {
         String personString = "João Scott Palmer Halpert Schrute Beesly  joao@fouyer.me      Brasil          São Paulo             Software Developer              ";
-        Person person = PersonPositional.build(personString); // I with this was available after first compile.
+        Person person = PersonPositional.build(personString); // I wish this was available after first compile.
         System.out.println(person.getCity()); //Should print São Paulo
     }
 }
